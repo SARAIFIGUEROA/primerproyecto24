0c556ea59b537c0014d642980f505454f1a0b690
SARAIFIGUEROA <Saraifigueroa012@gmail.com>
2024-05-06 09:33:58 -0300

    seguimos con iterfaces, usamos css, directiva e interpolacion de variables

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//importamos interfaz
+import { Buzo } from 'src/app/models/buzo';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+//propiedad publica tipo array
+public info: Buzo[];
+constructor() {
+  this.info = [
+    {
+      id: "",
+      nombre: "buzo",
+      tipo: "oversize",
+      valor: 100,
+      imagen: "https://falabella.scene7.com/is/image/FalabellaCO/126277319_1?wid=800&hei=800&qlt=70",
+      alt: "imagen",
+    },
+    {
+      id: "",
+      nombre: "remera",
+      tipo: "boxy",
+      valor: 100,
+      imagen: "https://acdn.mitiendanube.com/stores/943/997/products/remera-boy-over-negro1-04af4944190e9e96e716849377920935-640-0.jpg",
+      alt: "imagen",
+    }
 
+
+  ];
+}
 }
--- src/app/modules/inicio/pages/inicio1/inicio1.component.ts
+++ src/app/modules/inicio/pages/inicio1/inicio1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Buzo } from 'src/app/models/buzo';
+
+
 
 @Component({
   selector: 'app-inicio1',
@@ -7,28 +8,5 @@ import { Buzo } from 'src/app/models/buzo';
   styleUrls: ['./inicio1.component.css']
 })
 export class Inicio1Component {
-  //propiedad publica tipo array
-  public info: Buzo[];
-  constructor() {
-    this.info = [
-      {
-        id: "",
-        nombre: "buzo",
-        tipo: "oversize",
-        valor: 100,
-        imagen: "https://falabella.scene7.com/is/image/FalabellaCO/126277319_1?wid=800&hei=800&qlt=70",
-        alt: "imagen",
-      },
-      {
-        id: "",
-        nombre: "remera",
-        tipo: "boxy",
-        valor: 100,
-        imagen: "https://acdn.mitiendanube.com/stores/943/997/products/remera-boy-over-negro1-04af4944190e9e96e716849377920935-640-0.jpg",
-        alt: "imagen",
-      }
-
-
-    ];
-  }
+  
 }
