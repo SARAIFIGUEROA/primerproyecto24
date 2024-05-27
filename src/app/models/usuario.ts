889f8e0219278ce28181d3feb9dce776932dfb4f
SARAIFIGUEROA <saraifigueroa012@gmail.com>
2024-05-27 10:55:42 -0300

    interfaz usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid:string | any //atributos any reciben valores vacios o indefinidos
+    nombre: string;
+    apellido: string;
+    Email:string;
+    password:string; 
+}
