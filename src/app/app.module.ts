82f95fc712f18e1cb7695967232b8a7a6ef7d206
SARAIFIGUEROA <Saraifigueroa012@gmail.com>
2024-06-10 10:45:17 -0300

    servicios. email, password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 //importamos shared
 import { SharedModule } from './modules/shared/shared.module';
 
+//firebase
+import { enviroment } from 'src/enviroments/enviroment'; // vincula la BD a la app
+import { AngularFireModule } from '@angular/fire/compat'; // trabaja con las colecciones de la informacion
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // trabaja con la autentificacion
+import {AngularFireStorageModule } from '@angular/fire/compat/storage' // trabaja con imagenes y archivos
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './iniciosinuso/inicio.component';
@@ -20,6 +27,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    //firebase vinc
+    //
+    AngularFireModule.initializeApp(enviroment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
+//importamos servicio de autentificacion
+import { AuthService } from '../../services/auth.service';
+//importamos componentes de rutas de angular
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -21,32 +25,56 @@ export class RegistroComponent {
   }
   //Un arreglo de tipo usuario, significa que solo puede resibir valores que esten declarados en la intrerfaz usuario y de tipo array
   coleccionUsuarios: Usuario[] = []
+ 
+  constructor( public servicioAuth:AuthService,
+    public servicioRutas: Router
+  ){
 
-  //funcion para el regustro de nyevos usuarios
-  registrar() {
+  }
+  //funcion para el registro de nuevos usuarios
+   async registrar() {
     //constsnte credenciales va a resguardar la informacion que ingrese el usuario
-    const credenciales = {
+   /* const credenciales = {
       uid: this.usuarios.uid,
       nombre: this.usuarios.nombre,
       apellido: this.usuarios.apellido,
       email: this.usuarios.email,
       rol: this.usuarios.rol,
       password: this.usuarios.password,
+    }*/
+      //registro con servicio
+    const credenciales = {
+      email:this.usuarios.email,
+      password:this.usuarios.password
     }
-
+    const respuesta = this.servicioAuth.registro(credenciales.email, credenciales.password)
+//then es una promesa que devuelve el mismo valor 
+    .then(respuesta=>[
+      alert("Se pudo registrar con exito :)"),
+      //el metodo navigate nos direcciona a otra vista
+      this.servicioRutas.navigate(['/incio'])
+    ])
+    //encapsula un error. captura una falla y la vuelve error cuando la promesa salg mal
+    .catch(error => {
+      ("hubo un error al registrar un nuevo usuario : (\n"+error)
+    });
+  
+    /*
     //enviamos la informacion como uvo objeto a la coleccion
     this.coleccionUsuarios.push(credenciales);
-    //llamamos la funcion para ejecutarla
-    this.limpiarinputs();
+    //llamamos la funcion para ejecutarla*/
+alert('se ha registadp con exito')
+    
+    /*this.limpiarinputs();
     //mostramos credenciales por consola
     console.log(credenciales);
     console.log(this.coleccionUsuarios);
-  }
+  }*/
 
   //funcion para vaciar inputs
-  limpiarinputs() {
+  limpiarinputs(); /*{
     //en constante input llamamos atributos y los inicializamos
-    const input = {
+    /*const input = {
       uid: this.usuarios.uid='',
       nombre: this.usuarios.nombre='',
       apellido: this.usuarios.apellido='',
@@ -54,8 +82,6 @@ export class RegistroComponent {
       rol: this.usuarios.rol='',
       password: this.usuarios.password='',
 
-    }
-  }
-}
-
-
+    }*/
+  //}
+  }}
