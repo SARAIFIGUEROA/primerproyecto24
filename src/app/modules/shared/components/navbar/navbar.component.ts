30ad146e8bb7451dc6c9aadaa02099a399aa010c
SARAIFIGUREOA <Saraifigueroa012@gmail.com>
2024-08-27 10:46:49 -0300

    creación de modo oscuro con font awesome, usamos styles.css y navbar.html y nabvar.ts

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -27,4 +27,25 @@ export class NavbarComponent {
     this.servicioderutas.navigate(["./"]);
 
   }
+
+  //funcion para cambiar el cvolor de fondo para modo oscuro
+  cambiarFondo() {
+    //la variable toggle llama a un htmlinputelement que devuelve el valor del input a formato hmtl parapoder cambiarlo, tomamos el id y volvemos a usar el htmlinput para que nos devulva en html
+    let toggle: HTMLInputElement | null = document.getElementById('toggle') as HTMLInputElement
+    let label_toggle: HTMLElement | null = document.getElementById('label_toggle') as HTMLElement
+
+    if (toggle) {
+      let checked: boolean = toggle.checked;
+      document.body.classList.toggle('dark', checked)
+
+      if (checked) {
+        /*el inner nos devuelve un valor*/
+        //sol
+        label_toggle!.innerHTML = '<i class="fa-solid fa-sun"></i>';
+      } else {
+        //luna
+        label_toggle!.innerHTML = '<i class="fa-solid fa-moon"></i>'
+      }
+    }
+  }
 }
