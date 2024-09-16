234324af8fb3c2086ab355442a83aca7f035fc0e
ymndnss <yamnadenise05@gmail.com>
2024-09-16 10:51:35 -0300

    creamjos metodos 'obtenerurolimg' y 'subirimgt' correspondiente a conexion del sdervicio con storage, modificamos metodos ´crerproducto´ y eleminar producto para contemploar almacenamiento de storage, oldificaos metodo agregar producto, y creamos metodo cargarimg en eol compoobnent tabla

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from '../../services/crud.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -22,6 +22,10 @@ export class TableComponent {
 
   modalvisibleproductos: boolean = false;
 
+  //definimos variables para url de img y una para obtener la url de imagen de tipo string las dos
+  nombreurlimg!: string;
+  urlimg!: string;
+
   //definimos formuklarios para los productos 
   producto = new FormGroup({
     nombre: new FormControl('', Validators.required),
@@ -34,7 +38,7 @@ export class TableComponent {
 
   constructor(public serviciocrud: CrudService) { }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.serviciocrud.obtenerProducto().subscribe(producto => {
       this.coleccionproductos = producto;
     })
@@ -49,94 +53,124 @@ export class TableComponent {
         precio: this.producto.value.precio!,
         descripcion: this.producto.value.descripcion!,
         categoria: this.producto.value.categoria!,
-        imagen: this.producto.value.imagen!,
+        imagen: '',
         alt: this.producto.value.alt!,
       }
-
-      await this.serviciocrud.crearproducto(nuevoproducto)
-        .then(producto => {
-          Swal.fire({
-            title: "Felicidades!",
-            text: "Se pudo subir con exito!",
-            icon: "success"
-          });
-          //resetes el formulario y las casillas qeudan vacias
-          this.producto.reset();
+      //enviamos nombrey url de imagenes y definimjos carpeta de imagenes como "produtcos"
+      await this.serviciocrud.subirImagen(this.nombreurlimg, this.urlimg, "productos")
+        .then(resp => {
+          //encapsulamos la respuesta 
+          this.serviciocrud.obtenerurlimg(resp)
+            .then(url => {
+              //crearproducto recibe el njuevo producto y la url recibida
+              this.serviciocrud.crearproducto(nuevoproducto, url)
+                .then(producto => {
+                  Swal.fire({
+                    title: "Felicidades!",
+                    text: "Se pudo subir con exito!",
+                    icon: "success"
+                  });
+                  //resetes el formulario y las casillas qeudan vacias
+                  this.producto.reset();
+                })
+                .catch(error => {
+                  Swal.fire({
+                    title: "Error",
+                    text: "Hubo un error al subir producto!  \n" + error,
+                    icon: "error"
+                  });
+                  this.producto.reset();
+                });
+            })
         })
-
-        .catch(error => {
-          Swal.fire({
-            title: "Error",
-            text: "Hubo un error al subir producto!  \n" + error,
-            icon: "error"
-          });
-          this.producto.reset();
-        });
     }
   }
 
-//vinculamos con el modal
+  //creamos metodo caragr img
+  cargarImg(event: any) {
+    //variable para obtener el archivo subido desde el input del html
+    let archivo = event.target.files[0]; 
+//
+    let reader = new FileReader();
+    if (archivo != undefined) {
+      reader.readAsDataURL(archivo);
+
+      reader.onloadend = () => {
+        let url = reader.result;
+        if (url != null) {
+          this.nombreurlimg = archivo.name;
+          this.urlimg = url.toString();
+        }
+      }
+    }
+  }
+
+
+
+
+
+  //vinculamos con el modal
 
   MostrarBorrar(productoSeleccionado: Producto) {
     this.modalvisibleproductos = true;
     this.productoSeleccionado = productoSeleccionado;
   }
 
-  
-  borrarProducto(){
+
+  borrarProducto() {
     this.serviciocrud.eliminarProducto(this.productoSeleccionado.idproducto)
-    .then(respuesta => {
-      alert("Se ha podido eliminar con éxito.");
-    })
-    .catch(error => {
-      alert("Ha ocurrido un error al eliminar un producto: \n"+error);
-    })
-    
+      .then(respuesta => {
+        alert("Se ha podido eliminar con éxito.");
+      })
+      .catch(error => {
+        alert("Ha ocurrido un error al eliminar un producto: \n" + error);
+      })
+
   }
 
   //editar productos
-//va a recibir un producto seleccionado por el usuario, va a llamar al formularioi de producto y va a tomar uno por uno los atributos del formulario
-Mostrareditar(productoSeleccionado:Producto){
-this.productoSeleccionado = productoSeleccionado;
+  //va a recibir un producto seleccionado por el usuario, va a llamar al formularioi de producto y va a tomar uno por uno los atributos del formulario
+  Mostrareditar(productoSeleccionado: Producto) {
+    this.productoSeleccionado = productoSeleccionado;
 
-//todos los vlaores del producto seleccionado los va a autocompletar en el formulario del modal (menos el ID)
+    //todos los vlaores del producto seleccionado los va a autocompletar en el formulario del modal (menos el ID)
 
-this.producto.setValue({
-  nombre: productoSeleccionado.nombre,
-precio: productoSeleccionado.precio,
-descripcion:productoSeleccionado.descripcion,
-categoria:productoSeleccionado.categoria,
-imagen:productoSeleccionado.imagen,
-alt:productoSeleccionado.alt,
-})
-}
+    this.producto.setValue({
+      nombre: productoSeleccionado.nombre,
+      precio: productoSeleccionado.precio,
+      descripcion: productoSeleccionado.descripcion,
+      categoria: productoSeleccionado.categoria,
+      imagen: productoSeleccionado.imagen,
+      alt: productoSeleccionado.alt,
+    })
+  }
 
-//funcion editar producto
-//se vincula al boton editarProducto del modal editar
-EditarProducto(){
-  //recibbimos informacion nueva que el usuario ingrese y lo enciamos a la base de datos
-let datos: Producto ={
-  //solo Idproducto no se modirfica por el usuario
-  idproducto:this.productoSeleccionado.idproducto,
-  //los demas artributos recibiram nueva informacion desde el formulario
-nombre: this.producto.value.nombre!,
-precio:this.producto.value.precio!,
-descripcion:this.producto.value.descripcion!,
-categoria:this.producto.value.categoria!,
-imagen:this.producto.value.imagen!,
-alt:this.producto.value.alt!,
+  //funcion editar producto
+  //se vincula al boton editarProducto del modal editar
+  EditarProducto() {
+    //recibbimos informacion nueva que el usuario ingrese y lo enciamos a la base de datos
+    let datos: Producto = {
+      //solo Idproducto no se modirfica por el usuario
+      idproducto: this.productoSeleccionado.idproducto,
+      //los demas artributos recibiram nueva informacion desde el formulario
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      categoria: this.producto.value.categoria!,
+      imagen: this.producto.value.imagen!,
+      alt: this.producto.value.alt!,
 
-}
-//accedemos al servivio crud
-this.serviciocrud.editarProducto(this.productoSeleccionado.idproducto, datos)
-.then(producto=> {
-  alert("el producto se ha modificado con exito")
-  this.producto.reset();
-})
- 
-.catch(error=>{
-alert("hubo un problema al modificar un nuevo producto \n"+error)
-});
-this.producto.reset();
-}
+    }
+    //accedemos al servivio crud
+    this.serviciocrud.editarProducto(this.productoSeleccionado.idproducto, datos)
+      .then(producto => {
+        alert("el producto se ha modificado con exito")
+        this.producto.reset();
+      })
+
+      .catch(error => {
+        alert("hubo un problema al modificar un nuevo producto \n" + error)
+      });
+    this.producto.reset();
+  }
 }
--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -2,67 +2,134 @@ import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+import { getDownloadURL, getStorage, ref, UploadResult, uploadString, deleteObject } from 'firebase/storage';
+//funcionalidades de storage, importaciones para el manejo de archivos y referencias.
+//getDownloadURL es para tener la url de descarga de la imagen subida
+//getStorage es para obtener la instancia de almacenamiento
+//ref -> para crear referencias
+//Uploadresult -> Tipo que representa el resultado de una operacion subida
+//uploadString -> para subir imagenes en formato de cadena
+//deleteObjet -> para eliminar un espacio en el almacenamiento
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
   //definimos una coleccion para los productos de la web
-private productosCollection: AngularFirestoreCollection<Producto>
+  private productosCollection: AngularFirestoreCollection<Producto>
 
-  constructor(private database:AngularFirestore) { 
-    this.productosCollection=database.collection('producto');
+  //definimos variable respuesta que podra subir resultados
+  private respuesta!: UploadResult;
+
+  //inicializar servicio de storage
+
+  private storage = getStorage();
+
+
+
+
+  constructor(private database: AngularFirestore) {
+    this.productosCollection = database.collection('producto');
   }
 
-// crear,
-crearproducto(producto:Producto){
-return new Promise(async(resolve, reject)=> {
-  try{
-    //creamos numero identificativo para el producto en la base de datos
-    const idproducto=this.database.createId();
-//asignamos id creado al atributo idproducto de la interfaz Producto
-    producto.idproducto= idproducto;
-    //accede a la collecion productos
-    const resultado=await this.productosCollection.doc(idproducto).set(producto);
+  // crear,
+  crearproducto(producto: Producto, url: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        //creamos numero identificativo para el producto en la base de datos
+        const idproducto = this.database.createId();
+        //asignamos id creado al atributo idproducto de la interfaz Producto
+        producto.idproducto = idproducto;
+        //asignamos la url recibida
+        producto.imagen = url;
 
-    resolve(resultado);
-    }catch(error){
-      reject(error);
+        //accede a la collecion productos
+        const resultado = await this.productosCollection.doc(idproducto).set(producto);
+
+        resolve(resultado);
+      } catch (error) {
+        reject(error);
+      }
+    })
+  }
+
+
+  //funcion obtener 
+  //accedemos a la collecion productos
+  /*snapchanges toma captura del estado de los datos
+  pipe => tuberias que retornan un nuevo arreglo
+  map  "mapas" o recorre esa nueva informacion es un observador, lee, no hacve modificaciones por si solo a menos que se lo pidamos
+  a  resguarda la nueva informacion y la envia como un documento */
+  obtenerProducto() {
+    return this.productosCollection.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())))
+  }
+
+
+  //editar //id para saber cual es el producto que quiero eliminar
+  editarProducto(idproducto: string, nuevaData: Producto) {
+    return this.database.collection('producto').doc(idproducto).update(nuevaData);
+  }
+
+
+  // y eliminar producto
+  //id para saber cual es el producto que quiero eliminar
+  eliminarProducto(idproducto: string, imagenUrl: string) {
+    return new Promise((resolve, reject) => {
+      //encapsula todo lo que hace la promesa
+      try {
+        const storage = getStorage();
+        const referenciadeimg = ref(storage, imagenUrl);
+        deleteObject(referenciadeimg)
+
+          .then((res) => {
+            const respuesta = this.productosCollection.doc(idproducto).delete();
+            //deja la respuesta como resuelta
+            resolve(respuesta);
+          })
+
+          .catch(error => {
+            reject("Error al elkiminar la imagen: \n" + error);
+          })
+      }
+      catch (error) {
+        reject(error);
+      }
+    })
+  }
+
+
+  //funcion para obrtener la url de imagens
+  obtenerurlimg(respuesta: UploadResult) {
+    //retorna url obtenida como un a referencia
+    return getDownloadURL(respuesta.ref)
+  }
+  /**
+   * @param {string} nombre <- nombre de la imagen
+   * @param {any} imagen  <- tipo de imagenes  que se pueden subir
+   * @param {string} ruta  <-ruta de almacenamiento de laas imagenes
+   * @returns <- se retorna lo obtenido
+   */
+
+  //subir imagenes con sus referencias
+  async subirImagen(nombre: string,
+    imagen: any,
+    ruta: string,) {
+    try {
+      //accede a Storage (almacenamiento), ruta (crapeta) / nom re (nombre de la imagen)
+      let referenciadeimg = ref(this.storage, ruta + '/' + nombre);
+      //asignamos a la respuesta la informacion de las oimagenes subidas
+      this.respuesta = await uploadString(referenciadeimg, nombre, 'data_url')
+        .then(resp => {
+          return resp;
+        })
+      return this.respuesta;
+
+    } catch (error) {
+      console.log(error)
+      return this.respuesta;
     }
-})
-}
-
-
-//funcion obtener 
-//accedemos a la collecion productos
-/*snapchanges toma captura del estado de los datos
-pipe => tuberias que retornan un nuevo arreglo
-map  "mapas" o recorre esa nueva informacion es un observador, lee, no hacve modificaciones por si solo a menos que se lo pidamos
-a  resguarda la nueva informacion y la envia como un documento */
-obtenerProducto(){
-  return this.productosCollection.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())))
- }
-
-
-//editar //id para saber cual es el producto que quiero eliminar
-editarProducto(idproducto:string, nuevaData: Producto){
-return this.database.collection('producto').doc(idproducto).update(nuevaData);
-}
-
-
-// y eliminar producto
-//id para saber cual es el producto que quiero eliminar
-eliminarProducto(idproducto:string){
-return new Promise((resolve, reject) => {
-  //encapsula todo lo que hace la promesa
-  try {
-    const respuesta = this.productosCollection.doc(idproducto).delete();
-    //deja la respuesta como resuelta
-    resolve (respuesta);
-  }// en caso de que algo salga mal deja el catch
-  catch(error){
-    reject(error);
   }
-})
 }
-}
+
